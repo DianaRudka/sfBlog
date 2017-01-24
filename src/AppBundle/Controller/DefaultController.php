35f843f34fbea9a1e50349eabfa6732d30093ddc
Diana Rudka <diana.rudka@gmail.com>
2017-01-24 22:53:06 +0100

    Fixtures, pagination, adding comments

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Comment;
+use AppBundle\Entity\Post;
+use AppBundle\Form\CommentType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +16,52 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array());
+        $qb = $this->getDoctrine()
+                    ->getManager( )
+                    ->createQueryBuilder()
+                    ->select('p')
+                    ->from('AppBundle:Post', 'p');
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $qb,
+            $request->query->get('page', 1),
+            5
+        );
+
+        return $this->render('default/index.html.twig', array(
+            'posts' => $pagination
+        ));
+    }
+
+    /**
+     * @Route("/article/{id}", name = "post_show")
+     */
+    public function showAction(Post $post, Request $request)
+    {
+        $comment = new Comment();
+        $comment->setPost($post);
+
+//        $comment->setUser($user);
+
+        $form = $this->createForm(new CommentType(), $comment);
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', 'Komentarz poprawnie dodany');
+
+            return $this->redirectToRoute('post_show', array('id' => $post->getId()));
+
+        }
+
+        return $this->render('default/show.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView()
+        ));
     }
 }
--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace AppBundle\DataFixtures\ORM;
+
 use AppBundle\Entity\Post;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 /**
  * Description of LoadUserData
@@ -12,13 +14,21 @@ use Doctrine\Common\Persistence\ObjectManager;
  */
 class LoadUserData implements FixtureInterface {
 
-    
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create();
+
         for($i = 0; $i < 500; $i++) {
             $post = new Post();
-            $post->setTitle();
+            $post->setTitle($faker->sentence(4));
+            $post->setLead($faker->text(300));
+            $post->setContent($faker->text(700));
+            $post->setCreatedAt($faker->dateTimeThisMonth);
+
+            $manager->persist($post);
         }
+
+        $manager->flush();
     }
 
 }
